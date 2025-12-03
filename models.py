7cc9fecc6107f305bfa66a41dbe062432eed7698
Joy Chesire <chesirejoy39@gmail.com>
2025-12-03 18:31:30 +0300

    added the catalogues table

--- models.py
+++ models.py
@@ -8,11 +8,19 @@ Base = declarative_base()
 class Car (Base):
     __tablename__ = 'cars'
     
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     make = Column(String, nullable=False)
     model = Column(String, nullable=False)
     year = Column(Integer, nullable=False)
     price = Column(Float, nullable=False)
     mileage = Column(Float)
     color = Column(String)
+    created_at = Column(DateTime, default = datetime.now)
+
+class Catalogue (Base):
+    __tablename__ = 'catalogues'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False, unique=True)
+    description = Column(String)
     created_at = Column(DateTime, default = datetime.now)
